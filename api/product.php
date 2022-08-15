f55cda11c3b8050d17254e132752200fc26f571d
TFvrDev <nick.rius@gmail.com>
2022-08-15 15:25:28 +0200

    implemented better error handling (function) + wrote more checks + changed hint system

--- api/product.php
+++ api/product.php
@@ -2,6 +2,7 @@
 
 include('db.php');
 include('timer.php');
+include('error.php');
 
 if (isset($_GET['id'])) {
     getProduct();
@@ -10,7 +11,7 @@ if (isset($_GET['id'])) {
 } elseif (isset($_GET['hintproduct'])) {
     hintProduct();
 } else {
-    http_response_code(404);
+    sendErrorMessage(404, "Endpoint not found");
     exit();
 }
 
@@ -35,16 +36,15 @@ function getProduct()
         echo json_encode($json);
         exit();
     } catch (Exception $e) {
-        http_response_code(400);
-        echo "No valid productID";
+        sendErrorMessage(400, "No valid productID");
         exit();
     }
 }
 
 function checkProduct()
 {
-    // mitigate brute-force by time-out of 450ms (crude implementation)
-    usleep(450000);
+    // mitigate brute-force by time-out of 250ms (crude implementation)
+    usleep(250000);
 
     // check if session token is present
     if (isset($_COOKIE['sessionID'])) {
@@ -55,84 +55,83 @@ function checkProduct()
             $productId = htmlspecialchars($_GET['checkproduct']);
             $productEan = htmlspecialchars($_GET['ean']);
 
-            // get products in session array
-            global $conn;
-            $query = $conn->prepare("SELECT `session_products`, `session_found` FROM `sessions` WHERE `session_id`=:sesid");
-            $query->execute([
-                "sesid" => $id
-            ]);
-            $response = $query->fetch(PDO::FETCH_ASSOC);
-            $products = explode(",", $response['session_products']);
-            $foundProducts = explode(",", $response['session_found']);
+            // fetch the session
+            try {
+                // get products in session array
+                global $conn;
+                $query = $conn->prepare("SELECT `session_products`, `session_found` FROM `sessions` WHERE `session_id`=:sesid");
+                $query->execute([
+                    "sesid" => $id
+                ]);
+                $response = $query->fetch(PDO::FETCH_ASSOC);
+                $products = explode(",", $response['session_products']);
+                $foundProducts = explode(",", $response['session_found']);
 
-            // check if passed product is in session array
-            if (!in_array($productId, $products)) {
-                http_response_code(400);
-                echo "Product not in session set";
-                exit();
-            }
-
-            //check if passed product not already found
-            if (in_array($productId, $foundProducts)) {
-                http_response_code(400);
-                echo "Product already found";
-                exit();
-            }
-
-            // get the product EAN code
-            $query = $conn->prepare("SELECT `product_ean` FROM `products` WHERE `id`=:prodid");
-            $query->execute([
-                "prodid" => $productId
-            ]);
-            $reponse = $query->fetch(PDO::FETCH_ASSOC);
-            $checkEan = $reponse['product_ean'];
-
-            // check if passed EAN corresponds to actual EAN
-            // TODO: change to check last xxxx numbers of EAN instead of whole
-            if ($productEan != $checkEan) {
-                http_response_code(400);
-                echo "Product EAN incorrect";
-                exit();
-            }
-
-            // format products string
-            $fprodstr = "";
-            if (!$foundProducts[0] == "") {
-                foreach ($foundProducts as $found) {
-                    $fprodstr .= $found . ",";
+                // check if passed product is in session array
+                if (!in_array($productId, $products)) {
+                    sendErrorMessage(400, "Product not in session set");
+                    exit();
                 }
+
+                //check if passed product not already found
+                if (in_array($productId, $foundProducts)) {
+                    sendErrorMessage(400, "Product already found");
+                    exit();
+                }
+
+                // get the product EAN code
+                $query = $conn->prepare("SELECT `product_ean` FROM `products` WHERE `id`=:prodid");
+                $query->execute([
+                    "prodid" => $productId
+                ]);
+                $reponse = $query->fetch(PDO::FETCH_ASSOC);
+                $checkEan = $reponse['product_ean'];
+
+                // check if passed EAN corresponds to actual EAN
+                // TODO: change to check last xxxx numbers of EAN instead of whole
+                if ($productEan != $checkEan) {
+                    sendErrorMessage(400, "Product EAN incorrect");
+                    exit();
+                }
+
+                // format products string
+                $fprodstr = "";
+                if (!$foundProducts[0] == "") {
+                    foreach ($foundProducts as $found) {
+                        $fprodstr .= $found . ",";
+                    }
+                }
+                $fprodstr .= $productId;
+
+                // set the product to found
+                $query = $conn->prepare("UPDATE `sessions` SET `session_found`=:fprod WHERE `session_id`=:sesid");
+                $query->execute([
+                    "sesid" => $id,
+                    "fprod" => $fprodstr
+                ]);
+
+                // respond with json
+                $json = array(
+                    "sessionID" => $id,
+                    "foundProducts" => $fprodstr
+                );
+                header('Content-Type: application/json');
+                echo json_encode($json);
+                exit();
+            } catch (Exception $e) {
+                sendErrorMessage(400, "Invalid sessionID token");
+                exit();
             }
-            $fprodstr .= $productId;
-
-            // set the product to found
-            $query = $conn->prepare("UPDATE `sessions` SET `session_found`=:fprod WHERE `session_id`=:sesid");
-            $query->execute([
-                "sesid" => $id,
-                "fprod" => $fprodstr
-            ]);
-
-            // respond with json
-            $response = array(
-                "sessionID" => $id,
-                "foundProducts" => $fprodstr
-            );
-            header('Content-Type: application/json');
-            echo json_encode($response);
-            exit();
         } else {
-            http_response_code(400);
-            echo "Session has expired";
+            sendErrorMessage(400, "Session has expired");
             exit();
         }
     } else {
         // return error when no sessionID token is present
-        http_response_code(400);
-        echo "No sessionID token";
+        sendErrorMessage(400, "No sessionID token");
         exit();
     }
 }
-
-// TODO: implement saving which hints have been used
 
 function hintProduct()
 {
@@ -141,53 +140,74 @@ function hintProduct()
         $id = htmlspecialchars($_COOKIE['sessionID']);
         // check if the session is still valid
         if (checkGameTime($id)) {
-            // get variables
-            $productId = htmlspecialchars($_GET['checkproduct']);
 
-            // get the product location
-            global $conn;
-            $query = $conn->prepare("SELECT `product_location` FROM `products` WHERE `id`=:prodid");
-            $query->execute([
-                "prodid" => $productId
-            ]);
-            $reponse = $query->fetch(PDO::FETCH_ASSOC);
-            $prodLocation = $reponse['product_location'];
+            try {
+                // get the session hints
+                global $conn;
+                $query = $conn->prepare("SELECT `session_hints`, `session_products` FROM `sessions` WHERE `session_id`=:sesid");
+                $query->execute([
+                    "sesid" => $id
+                ]);
+                $response = $query->fetch(PDO::FETCH_ASSOC);
+                $hints = explode(",", $response['session_hints']);
+                $products = explode(",", $response['session_products']);
 
-            // get the session hints
-            $query = $conn->prepare("SELECT `session_hints` FROM `sessions` WHERE `session_id`=:sesid");
-            $query->execute([
-                "sesid" => $id
-            ]);
-            $response = $query->fetch(PDO::FETCH_ASSOC);
-            $hints = $response['session_hints'];
+                // get variables
+                $productId = htmlspecialchars($_GET['hintproduct']);
 
-            // increase the hint counter for the session
-            $hints += 1;
-            $query = $conn->prepare("UPDATE `sessions` SET `session_hints`=:hints WHERE `session_id`=:sesid");
-            $query->execute([
-                "sesid" => $id,
-                "hints" => $hints
-            ]);
+                // get the product location
+                $query = $conn->prepare("SELECT `product_location` FROM `products` WHERE `id`=:prodid");
+                $query->execute([
+                    "prodid" => $productId
+                ]);
+                $reponse = $query->fetch(PDO::FETCH_ASSOC);
+                $prodLocation = $reponse['product_location'];
 
-            // respond with json
-            $response = array(
-                "sessionID" => $id,
-                "hintsUsed" => $hints,
-                "productID" => $productId,
-                "productLocation" => $prodLocation
-            );
-            header('Content-Type: application/json');
-            echo json_encode($response);
-            exit();
+                // check if product is in session set
+                if (!in_array($productId, $products)) {
+                    sendErrorMessage(400, "Product not in session set");
+                    exit();
+                }
+
+                // check if queried product is already unlocked
+                if (!in_array($productId, $hints)) {
+                    // format hints string
+                    $hprodstr = "";
+                    if (!$hints[0] == "") {
+                        foreach ($hints as $hint) {
+                            $hprodstr .= $hint . ",";
+                        }
+                    }
+                    $hprodstr .= $productId;
+
+                    // set the product to found
+                    $query = $conn->prepare("UPDATE `sessions` SET `session_hints`=:hprod WHERE `session_id`=:sesid");
+                    $query->execute([
+                        "sesid" => $id,
+                        "hprod" => $hprodstr
+                    ]);
+                }
+
+                // respond with json
+                $json = array(
+                    "sessionID" => $id,
+                    "productID" => $productId,
+                    "productLocation" => $prodLocation
+                );
+                header('Content-Type: application/json');
+                echo json_encode($json);
+                exit();
+            } catch (Exception $e) {
+                sendErrorMessage(400, "Invalid sessionID token");
+                exit();
+            }
         } else {
-            http_response_code(400);
-            echo "Session has expired";
+            sendErrorMessage(400, "Session has expired");
             exit();
         }
     } else {
         // return error when no sessionID token is present
-        http_response_code(400);
-        echo "No sessionID token";
+        sendErrorMessage(400, "No sessionID token");
         exit();
     }
 }
